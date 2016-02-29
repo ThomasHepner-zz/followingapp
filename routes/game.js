b658de7b5fe474b161e0ee87c50fe6226afe696e
Thomas Hepner <thepner32@gmail.com>
2016-02-28 23:24:51 -0300

    Authentication

---
+++ routes/game.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+    var express = require('express');
+    var router = express.Router();
+
+    return router.get('/', function(req, res) {
+        if (!req.session.profile) {
+            res.redirect('/');
+        } else {
+            res.render('game', { title: 'Following', profile: req.session.profile })
+        }
+    });
+};
